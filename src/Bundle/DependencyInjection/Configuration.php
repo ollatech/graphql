ed4a6770571b40d704b1ce216b14ce0d2396b667
ollatech <olla.code@gmail.com>
2018-03-08 14:34:08 +0700

    global update

---
+++ src/Bundle/DependencyInjection/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+namespace Olla\Graphql\Bundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+
+
+final class Configuration implements ConfigurationInterface
+{
+
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('olla_graphql');
+        
+        return $treeBuilder;
+    }
+}
